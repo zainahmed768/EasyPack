108ad9cf29c4cc8f38964438439e5e36460a2b38
Zain Ahmed <zain.ahmed@inoviotech.local>
2022-09-06 20:36:29 +0500

    Will

--- inc/header.php
+++ inc/header.php
@@ -9,13 +9,16 @@
                     <div class="col-md-8">
                         <div class="nav-list">
                             <ul>
-                                <li><a href="index.php" >home</a></li>
-                                <li><a href="user-guide.php">About US </a></li>
+                                <li><a href="index.php">Home</a></li>
+
                                 <li><a href="shop.php">Shop</a></li>
                                 <li><a href="">Customization</a></li>
-                                <li><a href="">Our Collection</a></li>
-                                <li><a href="collection.php">Gallery</a></li>
-                                <li><a href="testimonials.php">Testimonials</a></li>
+                                <li><a href="">SOCIAL</a></li>
+                                <li><a href="user-guide.php">About US </a></li>
+                                <li><a href="#">CUSTOMER CARE</a></li>
+                                <!-- <li><a href="">Our Collection</a></li> -->
+                                <!-- <li><a href="collection.php">Gallery</a></li> -->
+                                <!-- <li><a href="testimonials.php">Testimonials</a></li> -->
                                 <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
                                 <li><a href="signin.php"><i class="fa fa-user" aria-hidden="true"></i> </a></li>
                                 <li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
@@ -32,10 +35,8 @@
 
         <div class="mobile-header">
             <div class="cancel">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                 </svg>
             </div>
             <ul class="mobile-nav">
