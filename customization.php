a5fbdc414f95541cf303b1cf4c9598dd209d5450
mshariqkhan12 <muhammad.shariq@inoviotech.com>
2022-12-09 21:44:30 +0500

    done

--- customization.php
+++ customization.php
@@ -34,7 +34,7 @@
              <div class="col-lg-5">
                  <div class="img-wrapper">
                      <figure>
-                         <img src="images/ccos.png" class="img-fluid" alt="">
+                         <img src="images/Easypack-pic.jpg" class="img-fluid" alt="">
                      </figure>
                  </div>
              </div>
